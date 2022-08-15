c48b434cbf27259dd51bf4fba5c5bddfcd693af3
Amilton Junior <amilton.od@gmail.com>
2022-08-15 14:19:41 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+x = float(input('Entre com o número: '))
+y = float(input('Entre com o número: '))
+
+soma = x + y
+print(f'Resultado: {soma}')
+
+sub = x - y
+print(f'Resultado: {sub}')
+
+div = x / y
+print(div)
+
+mult = x * y
+print(mult)
